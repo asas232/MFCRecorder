7db9d2e57721d62fc8619883e993619884526167
asas232 <1003413598@qq.com>
2019-06-24 21:11:42 +0800

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -135,4 +135,4 @@ def manage_file():
 @app.route('/down/<filename>', methods=['GET', 'POST'])
 def download(filename):
     # fname = filename.encode('cp936')
-    return send_from_directory(directory="/app/captures", filename=filename)
+    return send_from_directory(directory="/app/captures", filename=filename,as_attachment=True)
