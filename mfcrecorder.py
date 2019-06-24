aa28f3712d1a5514e4dccf12eb8b9e0c53968ecb
asas232 <1003413598@qq.com>
2019-06-24 13:54:38 +0800

    Update mfcrecorder.py

--- mfcrecorder.py
+++ mfcrecorder.py
@@ -8,28 +8,8 @@ import classes
 import webapp
 
 app=webapp.app;
-if __name__ == 'mfcrecorder':
-    #print("path-{}".format(sys.path[0]))
-    config = classes.config.Config('./config.conf')
-    print("xcx--{}".format(config))
-    #when config is edited at runtime and postprocessing is added, we cannot start it
-    if config.settings.post_processing_command:
-        classes.postprocessing.init_workers(config.settings.post_processing_thread_count)
-    if config.settings.web_enabled:
-        webapp.views.init_data(config)
-#         app.run(host='0.0.0.0')
-#         threading.Thread(
-#             target=webapp.app.run,
-#             kwargs={'host':'0.0.0.0', 'port':config.settings.port, 'threaded':'True', 'ssl_context':('certs/snakeoil.cert', 'certs/snakeoil.key')}
-#         ).start()
-     threading.Thread(
-          target=modelLoop,
-          args=(config)
-     ).start()
 
-  
-        
- def modelLoop(self,config):  
+def modelLoop(self,config):  
       next_run = datetime.datetime.now()
     while True:
         if datetime.datetime.now() < next_run:
@@ -44,3 +24,25 @@ if __name__ == 'mfcrecorder':
             classes.recording.start_recording(model.session, config)
             print("recording {}: {} ({} viewers) [{}]".format(model.name, model.session['uid'], model.session['rc'], model.tags))
         print('finished run')
+        
+if __name__ == 'mfcrecorder':
+    #print("path-{}".format(sys.path[0]))
+    config = classes.config.Config('./config.conf')
+    print("xcx--{}".format(config))
+    #when config is edited at runtime and postprocessing is added, we cannot start it
+    if config.settings.post_processing_command:
+        classes.postprocessing.init_workers(config.settings.post_processing_thread_count)
+    if config.settings.web_enabled:
+        webapp.views.init_data(config)
+#         app.run(host='0.0.0.0')
+#         threading.Thread(
+#             target=webapp.app.run,
+#             kwargs={'host':'0.0.0.0', 'port':config.settings.port, 'threaded':'True', 'ssl_context':('certs/snakeoil.cert', 'certs/snakeoil.key')}
+#         ).start()
+    threading.Thread(
+          target=modelLoop,
+          args=(config)
+      ).start()
+
+  
+
